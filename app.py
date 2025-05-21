1ac744c9c9a1db20454cc30124822ad03152f1e7
Maryam23129 <maryam.maryam@asdp.id>
2025-05-21 14:53:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,6 @@ if uploaded_tiket_files and uploaded_invoice and uploaded_summary and uploaded_r
     for col in ["Nominal Tiket Terjual", "Invoice", "Uang Masuk", "Selisih"]:
         formatted_df[col] = formatted_df[col].apply(lambda x: f"Rp {x:,.0f}" if isinstance(x, (int, float)) and x != 0 else "")
 
-    st.success("✅ Rekonsiliasi selesai! Tabel hasil berhasil dibuat.")
     st.subheader("📄 Tabel Rekapitulasi Hasil Rekonsiliasi")
     st.dataframe(formatted_df, use_container_width=True)
 
