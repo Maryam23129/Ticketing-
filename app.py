e2ee6cc123ba5f56a5fe088ec63b24e43ce48be7
Maryam23129 <maryam.maryam@asdp.id>
2025-05-21 15:59:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -157,8 +157,13 @@ if uploaded_tiket_files and uploaded_invoice and uploaded_summary and uploaded_r
     for col in ["Nominal Tiket Terjual", "Invoice", "Uang Masuk", "Selisih"]:
         formatted_df[col] = formatted_df[col].apply(lambda x: f"Rp {x:,.0f}" if isinstance(x, (int, float)) and x != 0 else "")
 
-    st.subheader("📄 Tabel Rekapitulasi Hasil Rekonsiliasi")
+    st.success("✅ Rekonsiliasi selesai! Tabel hasil berhasil dibuat.")
+    st.subheader("📄 Tabel Rekapitulasi Rekonsiliasi Per Pelabuhan")
     st.dataframe(formatted_df, use_container_width=True)
+
+    st.subheader("📄 Tabel Rekapitulasi Total Keseluruhan")
+    total_df = formatted_df.drop(columns=["Pelabuhan Asal"])
+    st.dataframe(total_df, use_container_width=True)
 
     output_excel = to_excel(df)
     st.download_button(
